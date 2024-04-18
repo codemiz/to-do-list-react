1af3408004cc55708c1fc8e6504b755e0761da87
NexusWarp <ik571572@gmail.com>
2024-04-18 22:44:54 +0500

    first

---
+++ src/App.jsx
@@ -0,0 +1,18 @@
+import React, { useState } from 'react';
+import './App.css';
+
+let array = [];
+
+function App() {
+  
+
+  
+
+  return (
+    <>
+      <div className='w-1/4 h-48 bg-green-600'></div>
+    </>
+  );
+}
+
+export default App;
