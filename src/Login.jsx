01db0ed86fe6d1d5a48a5eb9d799c038ea0c598c
NexusWarp <ik571572@gmail.com>
2024-04-19 20:28:51 +0500

    updated

--- src/Login.jsx
+++ src/Login.jsx
@@ -59,7 +59,7 @@ function Login() {
           <div>
             <button
               type="submit"
-              className="flex w-full justify-center rounded-md bg-fuchsia-400 px-3 py-1.5 text-sm font-semibold leading-6 text-white shadow-sm hover:bg-fuchsia-500 focus-visible:outline focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-indigo-600"
+              className="flex w-full justify-center rounded-md bg-blue-400 px-3 py-1.5 text-sm font-semibold leading-6 text-white shadow-sm hover:bg-fuchsia-500 focus-visible:outline focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-indigo-600"
             >
               Sign in
             </button>
--- src/Navbar.jsx
+++ src/Navbar.jsx
@@ -2,7 +2,7 @@ import React from 'react'
 
 function Navbar(props) {
   return (
-    <div className='bg-fuchsia-400 w-full h-14 flex justify-center items-center font-sans text-lg text-white'>
+    <div className='bg-slate-100 w-full h-14 flex justify-center items-center font-sans text-lg text-black'>
       <ul className='flex gap-16 font-semibold'>
         <li className='cursor-pointer hover:text-black'>{props.home}</li>
         <li className='cursor-pointer hover:text-black'>{props.one}</li>
