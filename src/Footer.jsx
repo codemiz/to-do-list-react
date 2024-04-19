66c73ebc17962d33eb89820baf57acd6797cead1
NexusWarp <ik571572@gmail.com>
2024-04-20 02:28:22 +0500

    updated

--- src/Footer.jsx
+++ src/Footer.jsx
@@ -3,7 +3,7 @@ import React from 'react'
 function Footer() {
   return (
     <div className=' bg-gray-200 flex justify-center fixed w-full bottom-0 text-black'>
-      <p> Copyright &copy; 2024!  <b>All rights reserved</b> </p>
+      <p> Copyright &copy; 2024! All rights reserved </p>
     </div>
   )
 }
