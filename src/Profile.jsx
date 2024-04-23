2ba1b8bd3fd95bc882222e5da0d37252f23f9609
NexusWarp <ik571572@gmail.com>
2024-04-24 00:56:03 +0500

    updated

--- src/Profile.jsx
+++ src/Profile.jsx
@@ -11,7 +11,8 @@ function logout(){
   navigate("/login");
 }
   if (!user) {
-    return <div>User not signed in</div>;
+    return <div className="font-semibold flex justify-center mt-4"><h3>Opps you're not signed in</h3></div>;
+
   }
   return (
     <div className="flex justify-center w-full pt-32 selection:bg-amber-400 ">
@@ -31,7 +32,7 @@ function logout(){
           <div className="flex gap-4 text-xl">
           <h4 className="font-bold">Email:</h4>
           <p className="font-semibold">{user.email}</p></div>
-          <button className="w-56 h-14 bg-amber-400 font-semibold rounded-md hover:bg-black hover:text-orange-300">Change email</button>
+          <button onClick={()=>alert("This feature is not available yet.")} className="w-56 h-14 bg-amber-400 font-semibold rounded-md hover:bg-black hover:text-orange-300">Change email</button>
         </div>
         <div
           id="task"
@@ -39,8 +40,8 @@ function logout(){
         >
           <div className="flex gap-4 text-x1">
           <h4 className="font-bold ">Total tasks saved:</h4>
-          <p className="font-semibold ">10</p></div>
-          <button className="w-56 h-14 bg-amber-400 font-semibold rounded-md hover:bg-black hover:text-orange-300">Add task</button>
+          <p className="font-semibold ">0</p></div>
+          <button onClick={()=> navigate("/")} className="w-56 h-14 bg-amber-400 font-semibold rounded-md hover:bg-black hover:text-orange-300">Add task</button>
         </div>
         <div
           id="task"
@@ -48,7 +49,7 @@ function logout(){
         >
           
           <button onClick={()=>logout()} className="w-64 h-14 bg-black text-white font-semibold hover:bg-zinc-700 rounded-md hover:text-white">Log out</button>
-          <button className="w-64 h-14 bg-amber-400  font-semibold hover:bg-black rounded-md hover:text-orange-300">Change password</button>
+          <button onClick={()=>alert("This feature is not available yet.")} className="w-64 h-14 bg-amber-400  font-semibold hover:bg-black rounded-md hover:text-orange-300">Change password</button>
         </div>
       </div>
     </div>
